ec46c8abacd551399abcd6c35a4e33182712a251
-k <slowdive@me.com>
2018-07-17 17:22:01 -0400

    move to wallet addr

--- README.md
+++ README.md
@@ -1,12 +1,114 @@
-# AXE WALLET GENERATOR _alpha_
+# AXE WALLET GENERATOR _v1.0.0_
 ```
-/__      ___ (  /     
-\\--`-'-|`---\\ | 
- |' _/   ` __/ /
- '._  W    ,--'      
-    |_:_._/
+/__      ___ (  /     
+\\--`-'-|`---\\ |
+ |' _/   ` __/ /
+ '._  W    ,--'      
+    |_:_._/
 ```
+JavaScript Client-Side AXE Wallet Generator
 
-# [Start](https://axerunners.github.io/axe-wallet-generator)
+Now AXE addresses and their corresponding private key can be conveniently
+generated in a web browser.
 
-based on https://github.com/MichaelMure/WalletGenerator.net
+## Installation
+
+1. Install dependencies for compiling single page application:
+
+    ```bash
+    npm install
+    ```
+
+2. Build the single-page application:
+
+    ```bash
+    npm run build
+    ```
+
+
+## Attribution
+
+Based on bitaddress.org
+JavaScript Client-Side Bitcoin Wallet Generator
+
+The bitaddress.org project provides an all-in-one HTML document with embedded
+JavaScript/Css/Images. The JavaScript is readable not minified and contains no
+XMLHttpRequest's (no AJAX). The benefit of this technique is you can load the
+JavaScript locally and trust that the JavaScript did not change after being
+loaded.
+
+Here is a link to the BitcoinTalk.org forum topic discussing original project:
+https://bitcointalk.org/index.php?topic=43496.0
+
+
+Please send DONATIONS for original project to Bitcoin Address:
+1NiNja1bUmhSoTXozBRBEtR8LeF9TGbZBN
+
+
+END USER NOTES:
+
+ 1) For Bulk Wallet I recommended using Google Chrome, it's the fastest.
+
+ 2) Requires IE9+, Firefox, Chrome or sufficient JavaScript support.
+
+ 3) Mobile Safari only works with iPhone4 or newer devices.
+    Older devices timeout while executing JavaScript.
+
+ 4) DO NOT use Opera Mini it renders JavaScript output server side, therefore
+    they might record the private key you generated.
+
+ 5) BIP38 most likely will not work on mobile devices due to hardware limitations.
+
+
+Notice of Copyrights and Licenses:
+---------------------------------------
+The wallet.axerunners.com project, software and embedded resources are
+copyright The AXE Developers.
+
+The wallet.axerunners.com name, AXE name and logo are not part of the open source
+license.
+
+The bitaddress.org project, software and embedded resources are
+copyright bitaddress.org.
+
+The bitaddress.org name and logo are not part of the open source
+license.
+
+Portions of the all-in-one HTML document contain JavaScript codes that
+are the copyrights of others. The individual copyrights are included
+throughout the document along with their licenses. Included JavaScript
+libraries are separated with HTML script tags.
+
+Summary of JavaScript functions with a redistributable license:
+
+JavaScript function  | License
+-------------------  | --------------
+Array.prototype.map  | Public Domain
+window.Crypto        | BSD License
+window.SecureRandom  | BSD License
+window.EllipticCurve | BSD License
+window.BigInteger    | BSD License
+window.QRCode        | MIT License
+window.Bitcoin       | MIT License
+
+The bitaddress.org software is available under The MIT License (MIT)
+Copyright (c) 2011-2013 bitaddress.org
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/culture/cs.js
@@ -0,0 +1,165 @@
+﻿(function (translator) {
+	translator.translations["cs"] = {
+		// javascript alerts or messages
+		"testneteditionactivated": "TESTNET aktivován",
+		"paperlabelbitcoinaddress": "AXE adresa:",
+		"paperlabelprivatekey": "Soukromý klíč:",
+		"paperlabelencryptedkey": "Šifrovaný soukromý klíč (Vyžadováno heslo)",
+		"bulkgeneratingaddresses": "Generuji adresy... ",
+		"brainalertpassphrasetooshort": "Zadané heslo je příliš krátké.\n\n",
+		"brainalertpassphrasewarning": "Varování: Je důležité zvolit silné heslo, které je odolné proti útoku hrubou silou a krádeži vašich AXEů.",
+		"brainalertpassphrasedoesnotmatch": "Heslo nejsou stejná.",
+		"detailalertnotvalidprivatekey": "Zadaný text není platým soukromým klíčem",
+		"detailconfirmsha256": "Zadaný text není platným soukromým klíčem!\n\nChcete použít zadaný text jako heslo a vytvořit soukromý klíč pomocí SHA256?\n\nVarování: Je důležité zvolit silné heslo, které je odolné proti útoku hrubou silou a krádeži vašich AXEů.",
+		"bip38alertincorrectpassphrase": "Špatné heslo pro BIP38",
+		"bip38alertpassphraserequired": "Vyžadováno heslo pro BIP38 klíč",
+		"vanityinvalidinputcouldnotcombinekeys": "Špatný vstup. Kombinovat klíče není možné.",
+		"vanityalertinvalidinputpublickeysmatch": "Špatný vstup. Veřejný klíč obou položek je shodný. Musíte zadat dva různé klíče.",
+		"vanityalertinvalidinputcannotmultiple": "Špatný vstup. Dva veřejné klíče není možné násobit. Zvolte 'Přidat' pro přidání dvou veřejných klíčů a získání AXE adresy.",
+		"vanityprivatekeyonlyavailable": "Dostupné pouze při kombinaci dvou soukromých klíčů",
+		"vanityalertinvalidinputprivatekeysmatch": "Špatný vstup. Soukromý klíč obou položek je shodný. Musíte zadat dva různé klíče.",
+
+		// header and menu html
+		"tagline": "Open Source generátor AXE peněženky napsaný v JavaScript",
+		"generatelabelbitcoinaddress": "Generuji AXE adresu",
+		"generatelabelmovemouse": "POHYBUJTE myší pro získání dostatku náhody...",
+		"generatelabelkeypress": "NEBO napište několik náhodných znaků do tohoto pole",
+		"singlewallet": "Jedna peněženka",
+		"paperwallet": "Papírová peněženka",
+		"bulkwallet": "Hromadná peněženka",
+		"brainwallet": "Myšlenková peněženka",
+		"vanitywallet": "Peněženka Vanity",
+		"splitwallet": "Split Wallet", //TODO: please translate
+		"detailwallet": "Detail peněženky",
+
+		// footer html
+		"footerlabeldonations": "Příspěvek:",
+		"footerlabeltranslatedby": "Překlad: 1LNF2anjkH3HyRKrhMzVYqYRKFeDe2TJWz",
+		"footerlabelpgp": "PGP",
+		"footerlabelversion": "Historie verzí",
+		"footerlabelgithub": "GitHub Repository",
+		"footerlabelgithubzip": "zip",
+		"footerlabelsig": "sig",
+		"footerlabelcopyright1": "Copyright bitaddress.org, The AXE Developers.",
+		"footerlabelcopyright2": "Copyright JavaScriptu je uveden ve zdrojovém kódu.",
+		"footerlabelnowarranty": "Bez záruky.",
+
+		// status html
+		"statuslabelcryptogood": "&#10004; Good!", //TODO: please translate
+		"statuslabelcryptogood1": "Your browser can generate cryptographically random keys using window.crypto.getRandomValues", //TODO: please translate
+		"statusokcryptogood": "OK", //TODO: please translate
+		"statuslabelcryptobad": "&times; Oh no!", //TODO: please translate
+		"statuslabelcryptobad1": "Your browser does NOT support window.crypto.getRandomValues. You should use a more modern browser with this generator to increase the security of the keys generated.",
+		"statusokcryptobad": "OK", //TODO: please translate
+		"statuslabelunittestsgood": "&#10004; Good!", //TODO: please translate
+		"statuslabelunittestsgood1": "All synchronous unit tests passed.", //TODO: please translate
+		"statusokunittestsgood": "OK", //TODO: please translate
+		"statuslabelunittestsbad": "&times; Oh no!", //TODO: please translate
+		"statuslabelunittestsbad1": "Some synchronous unit tests DID NOT pass. You should find another browser to use with this generator.", //TODO: please translate
+		"statusokunittestsbad": "OK", //TODO: please translate
+		"statuslabelprotocolgood": "&#10004; Good!", //TODO: please translate
+		"statuslabelprotocolgood1": "You are running this generator from your local computer. <br />Tip: Double check you are offline by trying ", //TODO: please translate
+		"statusokprotocolgood": "OK", //TODO: please translate
+		"statuslabelprotocolbad": "&#9888; Think twice!", //TODO: please translate
+		"statuslabelprotocolbad1": "You appear to be running this generator online from a live website. For valuable wallets it is recommended to", //TODO: please translate
+		"statuslabelprotocolbad2": "download", //TODO: please translate
+		"statuslabelprotocolbad3": "the zip file from GitHub and run this generator offline as a local html file.", //TODO: please translate
+		"statusokprotocolbad": "OK", //TODO: please translate
+		"statuslabelkeypool1": "This is a log of all the AXE Addresses and Private Keys you generated during your current session. Reloading the page will create a new session.", //TODO: please translate
+		"statuskeypoolrefresh": "Refresh", //TODO: please translate
+		"statusokkeypool": "OK", //TODO: please translate
+
+		// single wallet html
+		"newaddress": "Vytvořit novou adresu",
+		"singleprint": "Tisk",
+		"singlelabelbitcoinaddress": "AXE adresa",
+		"singlelabelprivatekey": "Soukromý klíč (WIF &naxe; Formát pro import do peněženky):",
+		"singletip1": "<b>AXE peněženka</b> je jednoduchý pár AXE adresy s přidruženým soukromým klíčem. Taková peněženka byla právě vytvořena ve vašem prohlížeči a zobrazena výše.",
+		"singletip2": "<b>Pro zabezpečení této peněženky</b> musíte tuto AXE adresu a soukromý klíč vytisknout a nebo jinak poznamenat. Je důležité provést zálohu soukromého klíče a jeho uschování na bezpečném místě. Tato webová stránka nemá žádné informace o vašem soukromém klíči. Pokud ovládáte PGP, můžete celou tuto stránku stáhnout v jednom HTML souboru a ověřit její pravost srovnáním SHA256 hashe s podepsaným dokumentem historie verzí. Odkaz naleznete v patičce této stránky. Pokud opustíte či obnovíte tuto stránku nebo kliknete na 'Vytvořit novou adresu' dojde k vygenerování nového soukromého klíče a předtím zobrazený klíč bude ztracen. Váš soukromý klíč musíte uchovat v tajnosti. Každý kdo má tento klíč k dispozici může utratit všechny peníze v této peněžence. Pokud budete peněženku tisknout, uzavřete ji do nepropustného obalu nebo ji zalaminujte. Tím zabráníte jejímu poškození vodou. Chovejte se k této peněžence jako k normálním bankovkám.",
+		"singletip3": "<b>Pro vložení</b> peněz do této peněženky stačí zaslat peníze na AXE adresu.",
+		"singletip4": "<b>Zkontrolovat zůstatek</b> můžete na webové stránce explorer.axe.org po zadání AXE adresy.",
+		"singletip5": "<b>Utratit AXE</b> můžete pomocí jednoho z P2P AXE klientů naimportováním soukromého klíče. Myslete na to, že importem klíče do klienta se stane součástí jeho peněženky. Pokud převedete někomu peníze, nespotřebovaný zůstatek se zašle na jinou AXE adresu uvedenou v P2P klienta. Tuto novou adresu musíte vyzálohovat a udržovat v bezpečí. Satoshi doporučuje, že by nikdo nikdy neměl mazat peněženku.",
+		"singleshare": "SDÍLEJTE",
+		"singlesecret": "SOUKROMÉ",
+
+		// paper wallet html
+		"paperlabelhideart": "Skrýt grafiku?",
+		"paperlabeladdressesperpage": "Adres na stránku:",
+		"paperlabeladdressestogenerate": "Vytvořit adres:",
+		"papergenerate": "Vytvořit",
+		"paperprint": "Tisk",
+		"paperlabelBIPpassphrase": "Heslo:",
+		"paperlabelencrypt": "Šifrovat BIP38?",
+
+		// bulk wallet html
+		"bulklabelstartindex": "Počátek:",
+		"bulklabelrowstogenerate": "Počet řádku k vytvoření:",
+		"bulklabelcompressed": "Komprimované adresy?",
+		"bulkgenerate": "Vytvořit",
+		"bulkprint": "Tisk",
+		"bulklabelcsv": "Čárkou oddělené hodnoty (CSV):",
+		"bulklabelformat": "Index, Adresa, Soukromý klíč (WIF &naxe; Formát pro import do peněženky)",
+		"bulklabelq1": "Proč bych měl používat Hromadnou peněženku pro příjem AXEů na mé stránce?",
+		"bulka1": "Tradiční způsob jak přijímat AXEy na vaší webové stránce vyžaduje instalaci oficiálního AXE klienta (\"axed\"). Mnoho webhostingových společností neumožňuje tuto instalaci provést. Také běh AXE démona na webovém serveru znamená, že soukromé klíče jsou uloženy na serveru a mohou být ukradeny. Pokud použijete Hromadnou peněženku, tak stačí na server nahrát pouze veřejnou AXE adresu a ne soukromé klíče. Poté se nemusíte bát, že vaše AXEy budou ukradeny v případě napadení serveru.",
+		"bulklabelq2": "Jakým způsobem mohou přijímat AXEy na mé stránce pomocí Hromadné peněženky?",
+		"bulklabela2li1": "Předgenerujte si velké množství AXE adres (10 000+). Okopírujte si CSV seznam do souboru na bezpečné místo ve vašem počítači. Poté jej vyzálohujte na bezpečné místo.",
+		"bulklabela2li2": "Naimportujte AXE adresy do databáze na vašem webovém serveru. Neimportujte soukromé klíče, abyste zabránili krádeži vašich peněz.",
+		"bulklabela2li3": "Umožněte na vaší stránce platbu pomocí AXEu. Stačí vždy zobrazit jednu z vygenerovaných adres a uložit si ji u objednávky.",
+		"bulklabela2li4": "Nyní je již pouze potřeba zařídit notifikace o příchozí transakci. Zadejte do Google \"AXE payment notification\" a využijte jednu z existujících služeb. Existuje jich několik a podporují např. Web Services, API, SMS, Email, apod. Notifikaci můžete zpracovat automaticky. Pro ruční kontrolu, zda peníze přišly, stačí použít Block Explorer. Nahraďte SEMPATŘÍADRESA AXE adresou, kterou chcete zkontrolovat. Potvrzení transkace může trvat od 10 minut do jedné hodiny.<br />http://explorer.axe.org/address/SEMPATŘÍADRESA<br /><br />Nepotvrzené tansakce je možné zkontrolovat na: https://bchain.info/AXE/ http://insight.axe.siampm.com/ <br />Většinou se zde zobrazí do 30 sekund.",
+		"bulklabela2li5": "AXEy budou bezpečně převedeny v řetězci bloků. Pro spotřebování stačí kdykoliv naimportovat soubor vygenerovaný v prvním kroku.",
+
+		// brain wallet html
+		"brainlabelenterpassphrase": "Zadejte heslo:",
+		"brainlabelshow": "Zobrazit?",
+		"brainprint": "Tisk",
+		"brainlabelconfirm": "Heslo znovu:",
+		"brainview": "Zobrazit",
+		"brainalgorithm": "Algoritmus: SHA256 (Heslo)",
+		"brainlabelbitcoinaddress": "AXE adresa:",
+		"brainlabelprivatekey": "Soukromý klíč (WIF &naxe; Formát pro import do peněženky):",
+
+		// vanity wallet html
+		"vanitylabelstep1": "Krok 1 &naxe; Vytvořte klíč pro první krok",
+		"vanitynewkeypair": "Vytvořit",
+		"vanitylabelstep1publickey": "Veřejný klíč 1. kroku",
+		"vanitylabelstep1pubnotes": "Zkopírujte a vložte výše uvedený klíč do pole Your-Part-Public-Key na Vanity Pool stránce.",
+		"vanitylabelstep1privatekey": "Soukromý klíč 1. kroku",
+		"vanitylabelstep1privnotes": "Zkopírujte a uschovejte uvedený soukromý klíč. Ideálně na šifrovaný disk. Budete ho potřebovat pro získání vašeho AXE soukromého klíče poté, co pool nalezne začátek.",
+		"vanitylabelstep2calculateyourvanitywallet": "Krok 2 &naxe; Výpočet peněženky Vanity",
+		"vanitylabelenteryourpart": "Zadejte vaši část soukromého klíče (vygenerovaný a uložený v prvním kroku výše):",
+		"vanitylabelenteryourpoolpart": "Zadejte pool část soukromého klíče (z Vanity Poolu):",
+		"vanitylabelnote1": "[POZNÁMKA: do tohoto pole můžete zadat veřejný nebo soukromý klíč]",
+		"vanitylabelnote2": "[POZNÁMKA: do tohoto pole můžete zadat veřejný nebo soukromý klíč]",
+		"vanitylabelradioadd": "Sečíst",
+		"vanitylabelradiomultiply": "Násobit",
+		"vanitycalc": "Spočítát peněženku Vanity",
+		"vanitylabelbitcoinaddress": "AXE adresa Vanity:",
+		"vanitylabelnotesbitcoinaddress": "Výše je vaše nová adresa, která by měla obsahovat požadovaný začátek.",
+		"vanitylabelpublickeyhex": "Veřejný klíč Vanity (HEX):",
+		"vanitylabelnotespublickeyhex": "Výše je veřejný klíč v hexadecimálním formátu.",
+		"vanitylabelprivatekey": "Soukromý klíč Vanity (WIF):",
+		"vanitylabelnotesprivatekey": "Výše je soukromý klíč pro načtení do vaší peněženky.",
+
+		// detail wallet html
+		"detaillabelenterprivatekey": "Zadejte soukromý klíč:",
+		"detailkeyformats": "Podporované formáty: WIF, WIFC, HEX, B64, B6, MINI, BIP38",
+		"detailview": "Zobrazit detail",
+		"detailprint": "Tisk",
+		"detaillabelnote1": "",
+		"detaillabelnote2": "",
+		"detaillabelbitcoinaddress": "AXE adresa:",
+		"detaillabelbitcoinaddresscomp": "Komprimovaná AXE adresa:",
+		"detaillabelpublickey": "Veřejný klíč (130 znaků [0-9A-F]):",
+		"detaillabelpublickeycomp": "Komprimovaný veřejný klíč (66 znaků [0-9A-F]):",
+		"detaillabelprivwif": "Soukromý klíč WIF<br />51 znaků v base58, začíná",
+		"detaillabelprivwifcomp": "Komprimovaný soukromý klíč WIF<br />52 znaků v base58, začíná",
+		"detaillabelprivhex": "Soukromý klíč v hexadecimálním formátů (64 znaků [0-9A-F]):",
+		"detaillabelprivb64": "Soukromý klíč v base64 (44 znaků):",
+		"detaillabelprivmini": "Soukromý klíč v mini formátů (22, 26 nebo 30 znaků, začíná 'S'):",
+		"detaillabelpassphrase": "Zadejte BIP38 heslo:",
+		"detailbip38decryptbutton": "Dešifrovat",
+		"detailbip38encryptbutton": "Encrypt BIP38", //TODO: please translate
+		"detaillabelq1": "Jak si mohu vytvořit peněženku pomocí hrací kostky? Co je to B6?",
+		"detaila1": "Důležitá součást vytváření AXE peněženky je jistota, že náhodná čísla použitá pro její tvorbu jsou opravdu náhodná. Fyzická náhoda je lepší než počítačem generovaná pseudonáhoda. Pomocí hrací kostky je možné jednoduše získat fyzicky náhodná čísla. Pro vytvoření soukromého klíče potřebujete pouze šestihrannou kostku, kterou 99x hodíte. Každý tento hod zaznamenejte. Při zapisování převeďte čísla takto: 1=1, 2=2, 3=3, 4=4, 5=5, 6=0. Pomocí této techniky zapisujete velké, opravdu náhodné číslo, svůj soukromý klíč v B6 nebo také base 6 formátu. Těchto 99 čísel napište do pole výše a klikněte na Zobrazit detail. Poté se vám zobrazí AXE adresa přidružená k tomuto soukromému klíči. Soukromý klíč byste si měli zaznamenat také ve WIF formátu, který je široce používán."
+	};
+})(ninja.translator);
